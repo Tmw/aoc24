148737416adda14c56f32b78f3dac93eb9267b69
Tiemen Waterreus <tiemen.waterreus@grafana.com>
2025-02-04 21:49:14 +0100

    Seems to be moving boxes correctly

--- day_15/README.md
+++ day_15/README.md
@@ -11,3 +11,10 @@ go run main.go < input.txt
 - Nothing really challenging, but fun assignment to work out and worth letting the animation play out in the terminal.
 - Detecting movable boxes in a chain was easy enough for part one by using recursion
 - Found the answer to part one in about 307µs
+- Part two is giving some issues => 1488820 is too high
+    - probably let it run again, writing out every frame.. See if we have odd behavior?
+    - perhaps we can run the example input again, see if that number still tracks? It doesn't..
+    - perhaps rewrite it so we can set the state like we want it (widened) and write test against certain scenario's in terms of moving,
+    - i think it now moves too few boxes around?
+    - moving away from simple grid system, getting the algorithm right when doing recursion
+    and trying to match up [ with ] is too much of a hassle. Will try out a free-form system instead?
--- day_15/main.go
+++ day_15/main.go
@@ -42,11 +42,164 @@ func (v Vector) Add(v2 Vector) Vector {
 	}
 }
 
+type ObjTyp int
+
+const (
+	ObjTypeWall = ObjTyp(iota)
+	ObjTypeBox
+)
+
+type Object struct {
+	width  int
+	height int
+	pos    Vector
+	typ    ObjTyp
+}
+
+func (o *Object) MoveInDirection(direction Vector) {
+	o.pos = o.pos.Add(direction)
+}
+
+type World struct {
+	width   int
+	height  int
+	objects []*Object
+}
+
+func (w World) Print(r *Robot) {
+	// make empty canvas
+	res := make([]rune, 0, w.width*w.height)
+	for y := 0; y < w.height; y++ {
+		for x := 0; x < w.width; x++ {
+			res = append(res, '.')
+		}
+	}
+
+	// draw in the objects
+	for _, obj := range w.objects {
+		var (
+			start = obj.pos.Y*w.width + obj.pos.X
+			end   = min(obj.pos.Y*w.width+obj.pos.X+obj.width, len(res))
+		)
+
+		for idx := start; idx < end; idx++ {
+			switch obj.typ {
+			case ObjTypeWall:
+				res[idx] = '#'
+			case ObjTypeBox:
+				if obj.width == 1 {
+					res[idx] = 'O'
+					continue
+				}
+
+				if idx == end-1 {
+					res[idx] = ']'
+					continue
+				}
+
+				if idx == start {
+					res[idx] = '['
+					continue
+				}
+			}
+		}
+
+	}
+
+	// draw in the robot
+	res[r.Y*w.width+r.X] = '@'
+
+	// draw each line
+	for i, c := range res {
+		if i > 0 && i%w.width == 0 {
+			fmt.Print("\n")
+		}
+
+		fmt.Print(string(c))
+	}
+
+	fmt.Println()
+}
+
+func (w World) InBounds(pos Vector) bool {
+	return pos.X >= 0 && pos.Y >= 0 && pos.X <= w.width && pos.Y <= w.height
+}
+
+func (w World) FindCollisions(obj1 *Object, direction Vector, ignoring []*Object) []*Object {
+	var collisions []*Object
+	for _, obj2 := range w.objects {
+		// no comparison with self
+		if obj1 == obj2 {
+			continue
+		}
+
+		// skip comparing against objects in ignoring list
+		if slices.Contains(ignoring, obj2) {
+			continue
+		}
+
+		// do objects intersect?
+		intersect := obj1.pos.X < obj2.pos.X+obj2.width &&
+			obj1.pos.X+obj1.width > obj2.pos.X &&
+			obj1.pos.Y < obj2.pos.Y+obj2.height &&
+			obj1.pos.Y+obj1.height > obj2.pos.Y
+
+		if intersect {
+			collisions = append(collisions, obj2)
+		}
+	}
+
+	for _, colliding := range collisions {
+		newPos := colliding.pos.Add(direction)
+		probe := &Object{width: colliding.width, height: colliding.height, pos: newPos}
+		moreCollissions := w.FindCollisions(probe, direction, []*Object{colliding})
+		collisions = append(collisions, moreCollissions...)
+	}
+
+	return collisions
+}
+
+// CanMove returns true if in the obj van move in direction
+// either because it find an empty space or the objects it collides with
+// are in turn movable too.
+func (w World) CanMove(obj *Object, direction Vector) bool {
+	collisions := w.FindCollisions(obj, direction, []*Object{})
+
+	for _, col := range collisions {
+		if col.typ == ObjTypeWall {
+			return false
+		}
+	}
+
+	return true
+}
+
+func (w *World) Clone() World {
+	return World{
+		width:   w.width,
+		height:  w.height,
+		objects: slices.Clone(w.objects),
+	}
+}
+
+// func (a *Arena) FindAllTilesOfType(typ Tile) []Vector {
+// 	var res []Vector
+// 	for idx, tile := range a.tiles {
+// 		if tile == typ {
+// 			x := idx % a.width
+// 			y := idx / a.width
+// 			res = append(res, Vector{X: x, Y: y})
+// 		}
+// 	}
+//
+// 	return res
+// }
+
 type Robot struct {
 	Vector
 }
 
-func (r Robot) clone() Robot {
+func (r Robot) Clone() Robot {
 	return Robot{r.Vector}
 }
 
@@ -54,144 +207,12 @@ func (r *Robot) Move(to Vector) {
 	r.Vector = to
 }
 
-type Tile rune
-
 const (
 	TileOpen       = '.'
 	TileNonMovable = '#'
 	TileMovable    = 'O'
 	TileRobot      = '@'
-
-	// for pt 2
-	TileMovableLeftHalf  = '['
-	TileMovableRightHalf = ']'
 )
-
-type Arena struct {
-	width  int
-	height int
-
-	tiles []Tile
-}
-
-func (a *Arena) clone() Arena {
-	return Arena{
-		width:  a.width,
-		height: a.height,
-		tiles:  slices.Clone(a.tiles),
-	}
-}
-
-func (a *Arena) Print(robot Robot) {
-	for y := 0; y < a.height; y++ {
-		for x := 0; x < a.width; x++ {
-			loc := Vector{X: x, Y: y}
-			tile := a.TileAt(loc)
-
-			if robot.Vector == loc {
-				tile = TileRobot
-			}
-
-			fmt.Printf("%s", string(tile))
-		}
-		fmt.Printf("\n")
-	}
-}
-
-func (a *Arena) FindAllTilesOfType(typ Tile) []Vector {
-	var res []Vector
-	for idx, tile := range a.tiles {
-		if tile == typ {
-			x := idx % a.width
-			y := idx / a.width
-			res = append(res, Vector{X: x, Y: y})
-		}
-	}
-
-	return res
-}
-
-func (a *Arena) TileAt(pos Vector) Tile {
-	return a.tiles[pos.Y*a.width+pos.X]
-}
-
-func (a *Arena) SetTileAt(pos Vector, t Tile) {
-	a.tiles[pos.Y*a.width+pos.X] = t
-}
-
-func (a *Arena) InBounds(pos Vector) bool {
-	return pos.X > 0 && pos.X < a.width &&
-		pos.Y >= 0 && pos.Y <= a.height
-}
-
-func (a *Arena) IsTileMovableInDirection(pos Vector, direction Vector) bool {
-	var (
-		newPos = pos.Add(direction)
-		tile   = a.TileAt(newPos)
-	)
-
-	if tile == TileOpen {
-		return true
-	}
-
-	if tile == TileNonMovable {
-		return false
-	}
-
-	if tile == TileMovable {
-		return a.IsTileMovableInDirection(newPos, direction)
-	}
-
-	var (
-		directionLeftOrRight = direction == DirectionLeft || direction == DirectionRight
-		directionUpOrDown    = direction == DirectionUp || direction == DirectionDown
-		leftOrRightBoxHalf   = tile == TileMovableLeftHalf || tile == TileMovableRightHalf
-	)
-
-	if directionLeftOrRight && leftOrRightBoxHalf {
-		// with double width boxes, pushing left or right isn't different than
-		// pushing the single boxes left and right.
-		return a.IsTileMovableInDirection(newPos, direction)
-	}
-
-	if directionUpOrDown && leftOrRightBoxHalf {
-		// ok this might be more weird?
-		//
-		// if this is the left part find the right part (to the right of this one..)
-		// return a.IsTileMovableInDirection(left part newpos) && a.IsTileMovableInDirection(right part newpos)..
-	}
-
-	panic("reached invalid state")
-}
-
-func (a *Arena) MoveTileInDirection(pos Vector, direction Vector) {
-	var (
-		newPos = pos.Add(direction)
-		tile   = a.TileAt(newPos)
-	)
-
-	if tile == TileNonMovable {
-		return
-	}
-
-	if tile == TileOpen {
-		a.SetTileAt(newPos, TileMovable)
-		a.SetTileAt(pos, TileOpen)
-	}
-
-	if tile == TileMovable {
-		a.MoveTileInDirection(newPos, direction)
-		// re-check the tile at the newPos
-		// if now movable, move to it.
-		//
-		// this ensures we can move entire chains of connected boxes.
-		tile = a.TileAt(newPos)
-		if tile == TileOpen {
-			a.SetTileAt(newPos, TileMovable)
-			a.SetTileAt(pos, TileOpen)
-		}
-	}
-}
 
 type Instruction rune
 
@@ -203,97 +224,102 @@ const (
 )
 
 func main() {
-	arena, robot, instructions := parseInput(os.Stdin)
-	arenaB, robotB := arena.clone(), robot.clone()
+	world, robot, instructions := parseInput(os.Stdin)
+	_ = instructions
+
+	world.Print(&robot)
+	// world2, robot2 := world.Clone(), robot.Clone()
 
 	start := time.Now()
-	fmt.Println("answer part one =", partOne(arena, robot, instructions))
+	fmt.Println("answer part one =", partOne(&world, &robot, instructions))
 	fmt.Printf("part one took %+v\n", time.Since(start))
 
-	applyWidening(&arenaB, &robotB)
-	arenaB.Print(robotB)
+	// applyWidening(&world2, &robot2)
+	// world2.Print(robot2)
 
-	start = time.Now()
-	fmt.Println("answer part two =", partTwo(arenaB, robotB, instructions))
-	fmt.Printf("part two took %+v", time.Since(start))
+	// start := time.Now()
+	// fmt.Println("answer part two =", partTwo(arenaB, robotB, instructions))
+	// fmt.Printf("part two took %+v", time.Since(start))
 }
 
-func applyWidening(a *Arena, r *Robot) {
+func applyWidening(w *World, r *Robot) {
+	_ = r
 	const wideningFactor = 2
-	newTiles := make([]Tile, 0, a.width*wideningFactor*a.height)
 
-	for _, t := range a.tiles {
-		if t == TileOpen || t == TileNonMovable {
-			newTiles = append(newTiles, []Tile{t, t}...)
-			continue
-		}
-
-		if t == TileMovable {
-			newTiles = append(newTiles, []Tile{TileMovableLeftHalf, TileMovableRightHalf}...)
-			continue
-		}
+	w.width *= wideningFactor
+	for idx := range w.objects {
+		w.objects[idx].pos.X *= wideningFactor
+		w.objects[idx].width *= wideningFactor
 	}
 
-	a.width *= wideningFactor
-	a.tiles = newTiles
 	r.Vector.X *= wideningFactor
 }
 
-func runInstructions(arena Arena, robot Robot, instructions []Instruction) {
+func runInstructions(world *World, robot *Robot, instructions []Instruction) {
 	for _, instr := range instructions {
+		fmt.Println("-------------------------------")
+		world.Print(robot)
+		fmt.Println("instruction = ", string(instr))
+
+		// time.Sleep(200 * time.Millisecond)
+
 		direction := vectorForInstruction(instr)
-
 		newPos := robot.Vector.Add(direction)
-		if !arena.InBounds(newPos) {
+
+		if !world.InBounds(newPos) {
 			continue
 		}
 
-		tile := arena.TileAt(newPos)
-		if tile == TileNonMovable {
+		// TODO: Add method for moving the robot? Expose through Vector?
+		// should Robot be just another Object in the world? :idea:
+		newRobot := &Object{pos: newPos, width: 1, height: 1}
+		if !world.CanMove(newRobot, direction) {
+			fmt.Println("unable to move, continuing..")
 			continue
 		}
 
-		if tile == TileOpen {
-			robot.Move(newPos)
-			continue
+		// TODO: Let's make it work, then improve:
+		// - perhaps CanMove can return a bool and a list of objects to move.
+		// - then calling move individually on them does the trick?
+		objsInPath := world.FindCollisions(newRobot, direction, []*Object{})
+		for _, o := range objsInPath {
+			o.MoveInDirection(direction)
 		}
 
-		if tile == TileMovable {
-			if !arena.IsTileMovableInDirection(newPos, direction) {
-				continue
-			}
-
-			arena.MoveTileInDirection(newPos, direction)
-			robot.Move(newPos)
-		}
+		robot.Move(newPos)
 	}
 }
 
-func partOne(arena Arena, robot Robot, instructions []Instruction) int {
-	runInstructions(arena, robot, instructions)
+func partOne(world *World, robot *Robot, instructions []Instruction) int {
+	applyWidening(world, robot)
+	runInstructions(world, robot, instructions)
+	fmt.Println("final state of the world:")
+	world.Print(robot)
 
-	boxPositions := arena.FindAllTilesOfType(TileMovable)
-	sum := 0
-	for _, p := range boxPositions {
-		sum += p.Y*100 + p.X
-	}
+	// boxPositions := world.FindAllTilesOfType(TileMovable)
+	// sum := 0
+	// for _, p := range boxPositions {
+	// 	sum += p.Y*100 + p.X
+	// }
 
+	var sum int
 	return sum
 }
 
-func partTwo(arena Arena, robot Robot, instructions []Instruction) int {
-	runInstructions(arena, robot, instructions)
-
-	// from the example it looked like only the distance from the left half was measured,
-	// ignoring the "closest edge" wording.
-	boxPositions := arena.FindAllTilesOfType(TileMovableLeftHalf)
-	sum := 0
-	for _, p := range boxPositions {
-		sum += p.Y*100 + p.X
-	}
-
-	return sum
-}
+//
+// func partTwo(arena Arena, robot Robot, instructions []Instruction) int {
+// 	runInstructions(arena, robot, instructions)
+//
+// 	// from the example it looked like only the distance from the left half was measured,
+// 	// ignoring the "closest edge" wording.
+// 	boxPositions := arena.FindAllTilesOfType(TileMovableLeftHalf)
+// 	sum := 0
+// 	for _, p := range boxPositions {
+// 		sum += p.Y*100 + p.X
+// 	}
+//
+// 	return sum
+// }
 
 type ParseType int
 
@@ -302,13 +328,14 @@ const (
 	ParseTypeInstructions
 )
 
-func parseInput(input io.Reader) (Arena, Robot, []Instruction) {
+func parseInput(input io.Reader) (World, Robot, []Instruction) {
 	var (
 		scanner      = bufio.NewScanner(input)
-		arena        Arena
+		world        World
 		robot        Robot
 		instructions []Instruction
 		parseType    = ParseTypeMap
+		lineNo       int
 	)
 
 	for scanner.Scan() {
@@ -325,22 +352,41 @@ func parseInput(input io.Reader) (Arena, Robot, []Instruction) {
 		// but this parses the map tiles, sets width and height and
 		// finds the initial position for the robot.
 		if parseType == ParseTypeMap {
-			arena.width = 0
-			for _, c := range line {
-				tile := Tile(c)
+			for idx, c := range line {
+				switch c {
+				case '#':
+					world.objects = append(world.objects, &Object{
+						width:  1,
+						height: 1,
+						typ:    ObjTypeWall,
+						pos: Vector{
+							X: idx,
+							Y: lineNo,
+						},
+					})
 
-				// detect robot, parse location but store tile as open
-				if tile == TileRobot {
-					robot.X = arena.width
-					robot.Y = arena.height
-					tile = TileOpen
+				case 'O':
+					world.objects = append(world.objects, &Object{
+						width:  1,
+						height: 1,
+						typ:    ObjTypeBox,
+						pos: Vector{
+							X: idx,
+							Y: lineNo,
+						},
+					})
+
+				case '@':
+					robot.X = idx
+					robot.Y = lineNo
 				}
 
-				arena.tiles = append(arena.tiles, tile)
-				arena.width++
+				world.width = idx + 1
 			}
 
-			arena.height++
+			lineNo++
+			world.height = lineNo
+
 			continue
 		}
 
@@ -352,5 +398,5 @@ func parseInput(input io.Reader) (Arena, Robot, []Instruction) {
 		}
 	}
 
-	return arena, robot, instructions
+	return world, robot, instructions
 }
